c55467a8ce3219c77c4c5ff5002c681c00596f4a
Pixelastic <tim@pixelastic.com>
2012-10-12 23:14:21 +1300

    Add a repetition? method.

--- keewee.rb
+++ keewee.rb
@@ -1,6 +1,16 @@
 # encoding : utf-8
 
+# Checks if a name is composed of several words
 def multiple_words?(city)
 	split = city.split(' ')
 	return split.length>1
 end
+
+# Checks if a word is made of a repetition
+def repetition?(city)
+	city = city.downcase
+	middle = city.length/2
+	return city[0,middle] == city[middle,4]
+end
+
+repetition?('Matamata')
--- tests/keewee.test.rb
+++ tests/keewee.test.rb
@@ -1,6 +1,6 @@
 # encoding : utf-8
 require 'test/unit'
-require '../keewee'
+require_relative '../keewee'
 
 class KeeweeTests < Test::Unit::TestCase
 
@@ -8,5 +8,10 @@ class KeeweeTests < Test::Unit::TestCase
 		assert_equal(true, multiple_words?('Te Aroha'))
 		assert_equal(true, multiple_words?('Palmertson North'))
 		assert_equal(false,multiple_words?('Wellington'))
+	end
+
+	def test_repetition?
+		assert_equal(true, repetition?('Matamata'))
+		assert_equal(false, repetition?('Eketahuna'))
 	end
 end
